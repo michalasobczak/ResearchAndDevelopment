a88ae37a50b9d38b9de97fc068b867c0cfe9202b
Michal Sobczak <michalasobczak@gmail.com>
2018-08-12 13:53:53 +0200

    test_addition, main benchmarking

--- RubyOnRails/Ruby/main.rb
+++ RubyOnRails/Ruby/main.rb
@@ -3,6 +3,7 @@
 #***************************
 load 'env\3d.rb'
 load 'env\2d.rb'
+require 'benchmark'
 
 
 #***************************
@@ -23,6 +24,7 @@ $canvasWidthHalf = 160
 # RUNTIME
 $vertices_counter=0
 vc = 0
+measures = []
 
 
 #***************************
@@ -54,38 +56,47 @@ cube_faces = [
 # PROGRAM
 #***************************
 #
-while true do
-  vc = 0
-  #
-  # VERTICES CALCULATION
-  for vi in (0..(cube_vertices.size-3)).step(3)
-    x2 = start_x+cube_vertices[vi+0]
-    y2 = start_y+cube_vertices[vi+1]
-    z2 = start_z+cube_vertices[vi+2]
-    point = transform_3_into_2(x2,y2,z2, camera_x,camera_y,camera_z, th)
-    point[:x] = point[:x].to_i
-    point[:y] = point[:y].to_i
-    put_pixel(point[:x]/10,point[:y]/10)
-    cube_2d[vc+0] = point[:x]
-    cube_2d[vc+1] = point[:y]
-    vc+=2
-  end # for 
-  #
-  # FACES  
-  initial_x = cube_2d[2]
-  initial_y = cube_2d[3] 
-  put_cursor_at(initial_x, initial_y)
-  for fi in (1..(cube_faces.size-1)).step(1)
-    fc = (cube_faces[fi]*2)-2;
-    x3 = cube_2d[fc+0]
-    y3 = cube_2d[fc+1]
-    print_line_to(x3,y3)
-  end # for
-  #
-  draw_screen
-  #
-  start_x = start_x + 10
-  start_y = start_y - 5
-  sleep 0.02
-  clear_screen
+100.times do
+  time = Benchmark.realtime do
+    vc = 0
+    #
+    # VERTICES CALCULATION
+    for vi in (0..(cube_vertices.size-3)).step(3)
+      x2 = start_x+cube_vertices[vi+0]
+      y2 = start_y+cube_vertices[vi+1]
+      z2 = start_z+cube_vertices[vi+2]
+      point = transform_3_into_2(x2,y2,z2, camera_x,camera_y,camera_z, th)
+      point[:x] = point[:x].to_i
+      point[:y] = point[:y].to_i
+      put_pixel(point[:x]/10,point[:y]/10)
+      cube_2d[vc+0] = point[:x]
+      cube_2d[vc+1] = point[:y]
+      vc+=2
+    end # for 
+    #
+    # FACES  
+    initial_x = cube_2d[2]
+    initial_y = cube_2d[3] 
+    put_cursor_at(initial_x, initial_y)
+    for fi in (1..(cube_faces.size-1)).step(1)
+      fc = (cube_faces[fi]*2)-2;
+      x3 = cube_2d[fc+0]
+      y3 = cube_2d[fc+1]
+      print_line_to(x3,y3)
+    end # for
+    #
+    draw_screen
+    #
+    start_x = start_x + 10
+    start_y = start_y - 5
+    #sleep 0.02
+    clear_screen
+  end # benchmark 
+  #puts time
+  measures << time
 end # while
+
+#***************************
+# BENCHMARK
+#***************************
+puts measures.inspect
---
+++ RubyOnRails/Ruby/test/benchmark.rb
@@ -0,0 +1,20 @@
+require 'minitest/autorun'
+require 'benchmark'
+
+N = 100_000_000
+
+
+class BenchmarkTest < Minitest::Test
+
+  def test_addition
+    i = 1
+    time = Benchmark.realtime do
+      N.times do
+        i = i + 2
+      end
+    end    
+    puts time.to_s
+    assert_equals i, 200_000_001
+  end # test_addition
+  
+end
