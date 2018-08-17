6735a04930c6b1eaadf47f65f6877b94ab8c56d1
Michal Sobczak <michalasobczak@gmail.com>
2018-08-17 21:36:28 +0200

    fix.

--- RubyOnRails/Ruby/test/benchmark_memory.rb
+++ RubyOnRails/Ruby/test/benchmark_memory.rb
@@ -1,11 +1,11 @@
 GC.start
 before = GC.stat(:total_freed_objects)
- 
+
 RETAINED = []
 100_000.times do
   RETAINED << "a string"
 end
- 
+
 GC.start
 after = GC.stat(:total_freed_objects)
-puts "Objects Freed: #{after – before}"
+puts "Objects Freed: #{after - before}"
