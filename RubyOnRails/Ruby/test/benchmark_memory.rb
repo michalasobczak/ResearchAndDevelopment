c5b32b11dbe91851baf6b8f7eb1e135d18515d64
Michal Sobczak <michalasobczak@gmail.com>
2018-08-17 19:56:35 +0200

    fill

--- RubyOnRails/Ruby/test/benchmark_memory.rb
+++ RubyOnRails/Ruby/test/benchmark_memory.rb
@@ -1,32 +1,11 @@
-require 'minitest/autorun'
-require 'benchmark'
-
-N = 100_000
-
-class BenchmarkMemoryTest < Minitest::Test
-
-  
-  def test_mem_arr
-    arr = []
-    i = 0
-    time = Benchmark.realtime do
-      N.times do
-        if i % 1_000_000 == 0 then puts i.to_s end
-        arr << 1
-        i = i + 1
-      end
-    end
-    puts 'test_mem_arr=' + time.to_s
-  end # test_mem_arr
-  
-  
-  def test_mem_str
-    time = Benchmark.realtime do
-      limit = N
-      str = "*" * limit   
-    end
-    puts 'test_mem_str=' + time.to_s
-  end # test_mem_str
-  
-   
+GC.start
+before = GC.stat(:total_freed_objects)
+ 
+RETAINED = []
+100_000.times do
+  RETAINED << "a string"
 end
+ 
+GC.start
+after = GC.stat(:total_freed_objects)
+puts "Objects Freed: #{after – before}"
