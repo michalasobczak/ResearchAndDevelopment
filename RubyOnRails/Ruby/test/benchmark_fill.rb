bb371c7594831e341c91d226eafeb58e37454ce4
Michal Sobczak <michalasobczak@gmail.com>
2018-08-17 22:29:28 +0200

    fill.

--- RubyOnRails/Ruby/test/benchmark_fill.rb
+++ RubyOnRails/Ruby/test/benchmark_fill.rb
@@ -6,20 +6,12 @@ puts mem_i.inspect
 N = 4_000_000_000
 arr = []
 i=0
-tmp_arr = [1,1,1,1,1,1,1,1,1,1,
-           1,1,1,1,1,1,1,1,1,1,
-           1,1,1,1,1,1,1,1,1,1,
-           1,1,1,1,1,1,1,1,1,1,
-           1,1,1,1,1,1,1,1,1,1]
 
 N.times do
   if i % 10_000_000 == 0 then
     tmp = GetProcessMem.new
     puts tmp.inspect
   end
-  arr << tmp_arr
+  arr << 1
   i+=1
 end
-
-mem_j = GetProcessMem.new
-puts mem_j.inspect
